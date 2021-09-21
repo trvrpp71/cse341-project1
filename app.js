ac539fd91fb1a653a1b113847869f1cc51828275
trevor pope <tpublic@popesonline.com>
2021-09-21 16:39:04 -0500

    additional prove02 work done

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res, next) => {
     res.render('index', {pageTitle: "Book List", books: books});

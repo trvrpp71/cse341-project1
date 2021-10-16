faa924c153b37c8de3e923b74714461865610716
trevor pope <tpublic@popesonline.com>
2021-10-16 12:21:02 -0500

    reverting back to previous commit

--- routes/proveRoutes/pr_index.js
+++ routes/proveRoutes/pr_index.js
@@ -5,13 +5,13 @@ const pr02Routes = require('./pr02/pr02Books');
 const pr03Routes = require('./pr03/pr03index');
 const pr04Routes = require('./pr04/pr04index');
 const pr05Routes = require('./pr05/pr05index');
-const pr06Routes = require('./pr06/pr06index');
+// const pr06Routes = require('./pr06/pr06index');
 
 pRoutes.use(pr02Routes);
 pRoutes.use(pr03Routes);
 pRoutes.use(pr04Routes);
 pRoutes.use(pr05Routes);
-pRoutes.use(pr06Routes);
+// pRoutes.use(pr06Routes);
 
 
 module.exports = pRoutes;
